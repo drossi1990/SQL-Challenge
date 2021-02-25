a1e13a2408986a1e0230d774ff4ec660ab7c3434
drossi1990 <70539961+drossi1990@users.noreply.github.com>
2021-02-24 18:26:43 -0800

    Add files via upload

---
+++ emp_queries.sql
@@ -0,0 +1,6 @@
+--1. employee salary information
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+INNER JOIN salaries
+ON employees.emp_no = salaries.emp_no
+ORDER BY employees.emp_no;

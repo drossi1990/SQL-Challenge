ba75ef374b62c11757425c3517566d2ef9edc8c4
drossi1990 <70539961+drossi1990@users.noreply.github.com>
2021-02-24 18:56:31 -0800

    Add files via upload

--- README.txt
+++ README.txt
@@ -4,3 +4,4 @@ Commit Log
 1. Initial commit
 2. database structure created and csv tables uploaded
 3. added primary, composite, and foreign keys
+4. First query and output file
--- emp_queries.sql
+++ emp_queries.sql
@@ -1,6 +1,13 @@
---1. employee salary information
+--1. Employee salary information
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 INNER JOIN salaries
 ON employees.emp_no = salaries.emp_no
 ORDER BY employees.emp_no;
+
+--2. Hired in 1986
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+
